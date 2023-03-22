8c575e8eab95500094be238d04b0e0dc1ec689a0
Lucas Leclerc <lucasleclerc74@gmail.com>
2023-03-22 16:53:00 +0000

    sujet 4 fini

--- code/23-NSI-04.py
+++ code/23-NSI-04.py
@@ -1,3 +1,24 @@
+#Exo 1:
+def a_doublon(tab):
+    """
+    regarde si il y a des doublons dans une liste
+    param: tab (list) liste de numéros trié
+    return: True ou false (bool)
+    """
+    assert len(tab) > 0, "liste vide"
+    for i in range(1, len(tab)):
+        if tab[i] == tab[i - 1]:
+            return True
+    return False
+
+
+#Test
+print("Test fonction a_doublon:")
+print(a_doublon([2, 5, 7, 7, 7, 9]))
+print(a_doublon([0, 2, 3]))
+
+
+#Exo 2:
 bombes = [(1, 1), (2, 4), (3, 1), (3, 3), (4, 4)]
 grille_test = [[1, 1, 1, 0, 0], [1, -1, 1, 1, 1], [2, 2, 3, 2, -1], [1, -1, 2, -1, 3], [1, 1, 2, 2, -1]]
 
@@ -13,10 +34,11 @@ def voisinage(n, ligne, colonne):
 
 def incrémente_voisins(grille, ligne, colonne):
     """ Incrémente de 1 toutes les cases voisines d'une bombe. """
-    voisins = ...
+    voisins = voisinage(len(grille), ligne, colonne)
     for l, c in voisins:
-        if grille[l][c] != ...: # si ce n'est pas une bombe
-            ...  		# on ajoute 1 à sa valeur
+        if grille[l][c] != -1: # si ce n'est pas une bombe
+            grille[l][c] += 1   # on ajoute 1 à sa valeur
+
 
 def génère_grille(bombes):
     """ Renvoie une grille de démineur de taille nxn où n est
@@ -26,10 +48,13 @@ def génère_grille(bombes):
     n = len(bombes)
     # Initialisation d'une grille nxn remplie de 0
     grille = [[0 for colonne in range(n)] for ligne in range(n)]
-
     # Place les bombes et calcule les valeurs des autres cases
     for ligne, colonne in bombes:
-        grille[ligne][colonne] = ...  # place la bombe
-        ...  # incrémente ses voisins
-
+        grille[ligne][colonne] = -1 # place la bombe
+        incrémente_voisins(grille, ligne, colonne)  # incrémente ses voisins
     return grille
+
+
+#Test
+print("\nTest fonction exo 2:")
+print(génère_grille(bombes))
