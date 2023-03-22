0ec08bf267e1ff5c7f182643f0c43180cb70ba1b
Lucas Leclerc <lucasleclerc74@gmail.com>
2023-03-22 14:00:16 +0000

    sujet 3 fini

--- code/23-NSI-03 2.py
+++ code/23-NSI-03 2.py
@@ -1,3 +1,29 @@
+#Exo 1:
+def moyenne(tab):
+    """
+    Calcule la moyenne des note de la liste
+    param: tab (liste) liste de tuple qui contient les notes et leurs coef
+    return: moyenne (float) ou None si coef egal 0
+    """
+    assert len(tab) > 0, "Liste vide"
+    total_coef = 0
+    total_note = 0
+    for note in tab:
+        total_note += note[0] * note[1]
+        total_coef += note[1]
+    if total_coef > 0:
+        return total_note / total_coef
+    else:
+        return None
+
+
+#Test
+print("Test fonction moyenne:")
+print(moyenne([(8, 2), (12, 0), (13.5, 1), (5, 0.5)]))
+print(moyenne([(3, 0), (5, 0)]))
+
+
+#Exo 2:
 coeur = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
          [0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
@@ -28,10 +54,10 @@ def affiche(dessin):
 def zoomListe(liste_depart, k):
     '''renvoie une liste contenant k fois chaque
        élément de liste_depart'''
-    liste_zoom = ...
-    for elt in ... :
+    liste_zoom = []
+    for elt in  liste_depart:
         for i in range(k):
-            ...
+            liste_zoom.append(elt)
     return liste_zoom
 
 def zoomDessin(grille, k):
@@ -39,7 +65,13 @@ def zoomDessin(grille, k):
        ET répétées k fois'''
     grille_zoom=[]
     for elt in grille:
-        liste_zoom = ...
+        liste_zoom = zoomListe(elt, k)
         for i in range(k):
-            ... .append(...)
+            grille_zoom.append(liste_zoom)
     return grille_zoom
+
+
+#Test
+print("Test fonction exo 2:")
+affiche(coeur)
+affiche(zoomDessin(coeur, 3))
