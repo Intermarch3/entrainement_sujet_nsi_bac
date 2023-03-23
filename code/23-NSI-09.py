cd20b95d4e3c3aea5f936efdf101540b57246ac2
Lucas Leclerc <lucasleclerc74@gmail.com>
2023-03-23 15:50:03 +0000

    sujet 9 et 10 fini

--- code/23-NSI-09.py
+++ code/23-NSI-09.py
@@ -1,14 +1,43 @@
-﻿# -*- coding: utf-8 -*-
+﻿#Exo 1:
+def multiplication(n1, n2):
+    total, n = 0, 0
+    while n != n2:
+        if n2 > 0:
+            total += n1
+            n += 1
+        else:
+            total -= n1
+            n -= 1
+    return total
 
+
+#Test
+print("Test fonction multiplication:")
+print(multiplication(3, 5))
+print(multiplication(-4, -8))
+print(multiplication(-2, 6))
+print(multiplication(-2, 0))
+
+
+#Exo 2:
 def chercher(tab, n, i, j):
     if i < 0 or j > len(tab):
         return None
     elif i > j:
         return None
-    m = (i + j) // ...
-    if ... < n:
-        return chercher(tab, n, ..., ...)
-    elif ... > n:
-        return chercher(tab, n, ..., ...)
+    m = (i + j) // 2
+    if tab[m] < n:
+        return chercher(tab, n, m, j)
+    elif tab[m] > n:
+        return chercher(tab, n, i, m)
     else:
-        return ...
+        return m
+
+
+#Test
+print("\nTestion fonction chercher")
+print(chercher([1, 5, 6, 6, 9, 12], 7, 0, 10))
+#Ne fonctionne pas pour cette exemple (impossible sans changer le programme de base)
+#print(chercher([1, 5, 6, 6, 9, 12], 7, 0, 5))
+print(chercher([1, 5, 6, 6, 9, 12], 9, 0, 5))
+print(chercher([1, 5, 6, 6, 9, 12], 6, 0, 5))
--- code/23-NSI-10.py
+++ code/23-NSI-10.py
@@ -1,3 +1,25 @@
+#Exo 1:
+def maxliste(tab):
+    """
+    Renvoie numéro max de la liste
+    param: tab (liste) liste de numéros entiers
+    return: max (int) numéro max
+    """
+    assert len(tab) > 0, "Liste vide"
+    max = tab[0]
+    for i in range(1, len(tab)):
+        if max < tab[i]:
+            max = tab[i]
+    return max
+
+
+#Test
+print("Test fonction maxliste:")
+print(maxliste([98, 12, 104, 23, 131, 9]))
+print(maxliste([-27, 24, -3, 15]))
+
+
+#Exo 2:
 class Pile:
     """
     Classe definissant une pile
@@ -31,14 +53,18 @@ def parenthesage(ch):
     """
     p = Pile()
     for c in ch:
-        if c == ...:
+        if c == "(":
             p.empiler(c)
-        elif c == ...:
+        elif c == ")":
             if p.est_vide():
-                return ...
+                return False
             else:
-                ...
+                p.depiler()
     return p.est_vide()
 
 
-
+#Test
+print("\nTest fonction parenthesage")
+print(parenthesage("((()())(()))"))
+print(parenthesage("())(()"))
+print(parenthesage("(())(()"))
